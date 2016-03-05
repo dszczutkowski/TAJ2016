581c73a09b7e7aeea0c78e816106b4784bb3c33f
dszczutkowski <dszczutkowski@outlook.com>
2016-03-05 19:04:52 +0100

    Dodanie projektu :octocat:

---
+++ Projekt1/Zad2/src/main/java/projekt1zad2/NieudanyPsikusException.java
@@ -0,0 +1,11 @@
+package projekt1zad2;
+
+public class NieudanyPsikusException extends Exception{
+	
+	private static final long serialVersionUID = 1L;
+
+	public NieudanyPsikusException()
+	{
+		System.out.println("NieudanyPsikus!");
+	}
+}
