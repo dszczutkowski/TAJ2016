1ba3bf567038aa6cff69a3b130ce7bf48ea2115b
dszczutkowski <dszczutkowski@outlook.com>
2016-03-06 17:47:22 +0100

    Dodanie metod

--- Projekt1/Zad2/src/main/java/projekt1zad2/NieudanyPsikusException.java
+++ Projekt1/Zad2/src/main/java/projekt1zad2/NieudanyPsikusException.java
@@ -1,11 +1,10 @@
 package projekt1zad2;
 
-public class NieudanyPsikusException extends Exception{
-	
+public class NieudanyPsikusException extends Exception {
+
 	private static final long serialVersionUID = 1L;
 
-	public NieudanyPsikusException()
-	{
+	public NieudanyPsikusException() {
 		System.out.println("NieudanyPsikus!");
 	}
 }
--- Projekt1/Zad2/src/main/java/projekt1zad2/PsikusImpl.java
+++ Projekt1/Zad2/src/main/java/projekt1zad2/PsikusImpl.java
@@ -1,5 +1,6 @@
 package projekt1zad2;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class PsikusImpl implements Psikus {
@@ -20,28 +21,105 @@ public class PsikusImpl implements Psikus {
 	}
 
 	public Integer CyfroKrad(Integer liczba) {
-		// TODO Auto-generated method stub
-		return null;
+		boolean ujemna = false;
+		if (liczba < 0)
+			ujemna = true;
+
+		liczba = Math.abs(liczba);
+		String liczbaToString = liczba.toString();
+
+		if (liczbaToString.length() <= 1)
+			return null;
+		else {
+			int pozycja = rnd.nextInt(liczbaToString.length());
+			StringBuilder sb = new StringBuilder(liczbaToString);
+			sb.deleteCharAt(pozycja);
+			int wynik = Integer.parseInt(sb.toString());
+
+			if (ujemna) {
+				return -wynik;
+			} else
+				return wynik;
+		}
 	}
 
 	public Integer HultajChochla(Integer liczba) throws NieudanyPsikusException {
-		// TODO Auto-generated method stub
-		return null;
+		boolean ujemna = false;
+		if (liczba < 0)
+			ujemna = true;
+		
+		liczba = Math.abs(liczba);
+		String liczbaStr = liczba.toString();
+		
+		if (liczbaStr.length() < 2)
+			throw new NieudanyPsikusException();
+		else {
+			StringBuilder sb = new StringBuilder(liczbaStr);
+			int p1 = rnd.nextInt(liczbaStr.length());
+			int p2;
+			do
+				p2 = rnd.nextInt(liczbaStr.length());
+			while (p2 == p1);
+
+			char znak1 = sb.charAt(p1);
+			char znak2 = sb.charAt(p2);
+			sb.setCharAt(p2, znak1);
+			sb.setCharAt(p1, znak2);
+			Integer wynik = Integer.parseInt(sb.toString());
+			
+			if (ujemna)
+				return -wynik;
+			return wynik;
+		}
 	}
 
 	public Integer Nieksztaltek(Integer liczba) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Integer> pozycje = new ArrayList<Integer>();
+		String liczbaStr = liczba.toString();
+		StringBuilder sb = new StringBuilder(liczbaStr);
+		
+		for (int i = 0; i < sb.length(); i++) {
+			if (sb.charAt(i) == '3' || sb.charAt(i) == '7' || sb.charAt(i) == '6')
+				pozycje.add(i);
+		}
+		
+		if (pozycje.size() == 0)
+			return Integer.parseInt(sb.toString());
+		else {
+			int p = rnd.nextInt(pozycje.size());
+			
+			if (sb.charAt(pozycje.get(p)) == '3')
+				sb.setCharAt((pozycje.get(p)), '8');
+			else if (sb.charAt(pozycje.get(p)) == '7')
+				sb.setCharAt((pozycje.get(p)), '1');
+			else if (sb.charAt(pozycje.get(p)) == '6')
+				sb.setCharAt((pozycje.get(p)), '9');
+
+			return Integer.parseInt(sb.toString());
+		}
 	}
 
 	public Integer Heheszki(Integer liczba) {
-		// TODO Auto-generated method stub
-		return null;
+		if (liczba == 0)
+			return 0;
+
+		boolean ujemna = false;
+		if (liczba < 0)
+			ujemna = true;
+
+		liczba = Math.abs(liczba);
+
+		if (ujemna)
+			return -(Integer) rnd.nextInt((int) liczba);
+		else
+			return (Integer) rnd.nextInt((int) liczba);
 	}
 
 	public boolean Titit(Integer liczba_dziel) {
-		// TODO Auto-generated method stub
-		return false;
+		if (liczba % liczba_dziel == 0)
+			return true;
+		else
+			return false;
 	}
 
 }
