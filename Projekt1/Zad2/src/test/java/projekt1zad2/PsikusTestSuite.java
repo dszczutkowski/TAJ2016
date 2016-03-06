58cffd1e084c450c9d0ead83a9341b8ca14080da
dszczutkowski <dszczutkowski@outlook.com>
2016-03-06 18:05:00 +0100

    dodanie suite :octocat:

---
+++ Projekt1/Zad2/src/test/java/projekt1zad2/PsikusTestSuite.java
@@ -0,0 +1,12 @@
+package projekt1zad2;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ CyfroKradTest.class, HeheszkiTest.class, HultajChochlaTest.class, NieksztaltekTest.class,
+		TititTest.class })
+
+public class PsikusTestSuite {
+
+}
