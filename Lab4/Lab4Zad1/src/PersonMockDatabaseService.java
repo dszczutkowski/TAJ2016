d71fee53bd7728ba6a8e286ba006e9aa33c08792
dszczutkowski <dszczutkowski@outlook.com>
2016-03-13 21:40:36 +0100

    usunieto stare zadanie

--- Lab4/Lab4Zad1/src/PersonMockDatabaseService.java
+++
@@ -1,9 +0,0 @@
-
-
-public interface PersonMockDatabaseService {
-  boolean AddPerson(Person person) throws PersonException;
-  boolean remove(int id) throws PersonException;
-  boolean read(int id) throws PersonException;
-  boolean update(int id) throws PersonException;
-}
-
